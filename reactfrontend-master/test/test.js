1d7dc224abec8aee2835e574a0a7afa91fdaea0d
mathumitha.p <mathumitha.p@payoda.com>
2017-09-13 13:42:22 +0530

    #3016

---
+++ reactfrontend-master/test/test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var validateLength = require('../src/validateFolder/validate');
+
+describe('Length Validation', function() {
+  it('input field length should be less than or equal to 10', function() {
+    var initialVal = new validateLength("samplestri");
+    expect(initialVal.checkLength()).to.be.at.most(10);
+  });
+});
